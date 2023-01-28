cb33c221b37ed2d0369444728e7a918aeda54f55
takahiro <takapi854@gmail.com>
2023-01-28 23:11:43 +0900

    push

--- modules/ecs/aws_ecs_task_definition.tf
+++ modules/ecs/aws_ecs_task_definition.tf
@@ -23,8 +23,8 @@ resource "aws_ecs_task_definition" "backend" {
       essential = true
       portMappings = [
         {
-          containerPort = 8080
-          hostPort      = 8080
+          containerPort = 80
+          hostPort      = 80
         }
       ]
     }
