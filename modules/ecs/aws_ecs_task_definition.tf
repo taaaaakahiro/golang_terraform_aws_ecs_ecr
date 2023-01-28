4ff8983e8f7ae2cc62cd6225c352e0e14fce6a00
revenue-hack <K.odeveloper10@gmail.com>
2023-01-28 21:40:08 +0900

    add healthcheck

--- modules/ecs/aws_ecs_task_definition.tf
+++ modules/ecs/aws_ecs_task_definition.tf
@@ -13,6 +13,12 @@ resource "aws_ecs_task_definition" "backend" {
       image  = "413937037843.dkr.ecr.ap-northeast-1.amazonaws.com/dev-ecr:latest"
       cpu    = 128
       memory = 256
+      healthcheck = {
+        command = [ "CMD-SHELL", "curl -f http://127.0.0.1:9090/health || exit 1" ]
+        interval = 20
+        retries = 2
+        timeout = 5
+      }
 
       essential = true
       portMappings = [
